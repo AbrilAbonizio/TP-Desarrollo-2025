0cdb1d11da97f1f404ff947790843fb0700aab24
Abril Abonizio <abril.abonizio@gmail.com>
2025-10-16 23:19:51 -0300

    Pasajero Routes
    Se agrega pasajero.routes.ts y app.use

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
-import express, { NextFunction, Request, Response } from 'express';
-import { ciudadRouter } from './Ciudad/ciudad.routes.js';
-=======
 import express from 'express';
 import { ciudadRouter } from './Ciudad/ciudad.routes.js';
-import { categoriaRouter } from './Categoria/categoria.routes.js';
-import { viajeRouter } from './Viaje/viaje.routes.js';
->>>>>>> Stashed changes
+import { pasajeroRouter } from './Pasajero/pasajero.routes.js';
 
 const app = express();
 
@@ -14,17 +8,10 @@ app.use(express.json()); // Middleware para parsear json
 
 app.use('/api/ciudades', ciudadRouter); // Usa ciudadRouter para manejar todas las peticiones que llegan a /api/ciudades
 // Va una linea para agregar una nueva CRUD
-<<<<<<< Updated upstream
+app.use('/api/pasajeros', pasajeroRouter);
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resourse not found' });
-=======
-app.use('/api/categorias', categoriaRouter); // Usa categoriaRouter para manejar todas las peticiones que llegan a /api/categorias
-app.use('/api/viajes', viajeRouter); // Usa categoriaRouter para manejar todas las peticiones que llegan a /api/categorias
-
-app.use((_, res) => {
-  return res.status(404).send({ message: 'RECURSO NO ENCONTRADO' });
->>>>>>> Stashed changes
 });
 
 app.listen(3000, () => {
