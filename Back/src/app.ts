cd388aaefd465b4844153b4f3c005a713e3f46a6
Abril Abonizio <abril.abonizio@gmail.com>
2025-10-16 23:02:30 -0300

    Pasajero Entity
    entity.ts + .http

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 import express, { NextFunction, Request, Response } from 'express';
 import { ciudadRouter } from './Ciudad/ciudad.routes.js';
+=======
+import express from 'express';
+import { ciudadRouter } from './Ciudad/ciudad.routes.js';
+import { categoriaRouter } from './Categoria/categoria.routes.js';
+import { viajeRouter } from './Viaje/viaje.routes.js';
+>>>>>>> Stashed changes
 
 const app = express();
 
@@ -7,9 +14,17 @@ app.use(express.json()); // Middleware para parsear json
 
 app.use('/api/ciudades', ciudadRouter); // Usa ciudadRouter para manejar todas las peticiones que llegan a /api/ciudades
 // Va una linea para agregar una nueva CRUD
+<<<<<<< Updated upstream
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resourse not found' });
+=======
+app.use('/api/categorias', categoriaRouter); // Usa categoriaRouter para manejar todas las peticiones que llegan a /api/categorias
+app.use('/api/viajes', viajeRouter); // Usa categoriaRouter para manejar todas las peticiones que llegan a /api/categorias
+
+app.use((_, res) => {
+  return res.status(404).send({ message: 'RECURSO NO ENCONTRADO' });
+>>>>>>> Stashed changes
 });
 
 app.listen(3000, () => {
